8bc3ac278dd0c3d649a0b63d5dac24d495eb8ca3
Fasciotti <srdrone@hotmail.com>
2025-05-09 23:01:36 -0300

    Add bullet impact VFX and minor adjustments on bullet

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -3,13 +3,27 @@ using UnityEngine;
 public class Bullet : MonoBehaviour
 {
     private Rigidbody rb => GetComponent<Rigidbody>();
+    private TrailRenderer trail => GetComponentInChildren<TrailRenderer>();
 
-    private void OnCollisionEnter()
-    {    
-        // rb.constraints = RigidbodyConstraints.FreezeAll;
+    [SerializeField] private GameObject bulletImpactFX;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        CreateImpactFX(collision);
+        trail.transform.parent = null;
         Destroy(gameObject);
+
     }
 
+    private void CreateImpactFX(Collision collision)
+    {
+        if (collision.contactCount > 0)
+        {
+            ContactPoint contact = collision.GetContact(0);
 
+            GameObject newImpactFX = Instantiate(bulletImpactFX, contact.point, Quaternion.LookRotation(contact.normal));
 
+            Destroy(newImpactFX, 1f);
+        }
+    }
 }
