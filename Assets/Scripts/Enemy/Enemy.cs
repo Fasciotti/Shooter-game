f0b33e8f1e019545f03939779b874c30d707542d
Fasciotti <srdrone@hotmail.com>
2025-06-17 17:22:50 -0300

    Add a StateMachine base to enemy

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Enemy : MonoBehaviour
+{
+    [Header("Idle Info")]
+    public float idleTime;
+    public EnemyStateMachine stateMachine { get; private set; }
+
+
+    protected virtual void Awake()
+    {
+        stateMachine = new EnemyStateMachine();
+
+    }
+
+    protected virtual void Start()
+    {
+
+    }
+
+    protected virtual void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Enemy/Enemy_Melee.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class Enemy_Melee : Enemy
+{
+    public MoveState_Melee moveState {  get; private set; }
+    public IdleState_Melee idleState {  get; private set; }
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+
+        idleState = new IdleState_Melee(this, stateMachine, "idle");
+        moveState = new MoveState_Melee(this, stateMachine, "move");
+
+        
+    }
+
+    protected override void Start()
+    {
+        base.Start();
+
+        stateMachine.Initialize(idleState);
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+
+        stateMachine.currentState.Update();
+    }
+}
